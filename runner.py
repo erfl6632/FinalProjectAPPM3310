ef42941c4df57a925d76cf26a5b88a5090cdae03
Wyett Considine <43249892+WyettConsidine@users.noreply.github.com>
2020-12-02 18:09:03 -0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -3,14 +3,40 @@ import decomps
 import numpy as np
 
 
-array = readCSV.readCSV('test.csv')
+array = readCSV.readCSV('test2.csv')
 farray = decomps.strgToFloat(array)
 #print(farray)
-#print(decomps.QRDecomp(farray))
+
+k = decomps.approxDim(farray,99,0)
+print(k)
+
+
+
+'''
+Y = decomps.randApprox(farray, k)
+print(Y)
+'''
+
+
+#QR = decomps.SVDDecomp(farray)
+#rank = np.linalg.matrix_rank(farray, 1.0e-10)
+#print(rank)
 #print(decomps.EigDecomp(farray))
 
 '''
 print(q)
 print(r)
 print(np.dot(q,r)) 
+'''
+
+'''
+ranks = np.linalg.matrix_rank(farray)
+for i in range(ranks):
+    e = np.linalg.matrix_rank(farray)-1-i
+    newRank = decomps.approxDim(farray,e,0)
+    #print(ranks)
+    #print(newRank)
+    if(newRank != ranks):
+        print(str(newRank) + " , " + str(e))
+        rank = newRank
 '''
