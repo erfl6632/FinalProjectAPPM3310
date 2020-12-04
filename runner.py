09596599b83963d089bb16665d02d066c2774bfe
Wyett Considine <43249892+WyettConsidine@users.noreply.github.com>
2020-12-04 15:35:10 -0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -3,40 +3,40 @@ import decomps
 import numpy as np
 
 
-array = readCSV.readCSV('test2.csv')
+array = readCSV.readCSV('28x11Fib.csv')
 farray = decomps.strgToFloat(array)
-#print(farray)
+#print(np.dot(decomps.QRDecomp(farray)[0],decomps.QRDecomp(farray)[0].T))
+#a1 = np.array([[1,0,0],[0,1,0],[0,0,1]])
+#a2 = np.array([[1,2,3],[2,4,6],[7,8,9]])
+#print(np.dot(a1,a2))
+'''
+print(decomps.SVDDecomp(farray)[1])
+print(np.linalg.matrix_rank(farray,1.0e-20))
 
-k = decomps.approxDim(farray,99,0)
-print(k)
-
+Q = decomps.approxGivenMat(farray,4.0e-7,0)
+print(Q)
+print(decomps.findError(farray,Q))
+'''
 
 
 '''
-Y = decomps.randApprox(farray, k)
-print(Y)
+X = np.dot(Q,Q.T)
+X = np.dot(X,farray)
+E = farray-X
+print(np.linalg.norm(E))
 '''
-
-
 #QR = decomps.SVDDecomp(farray)
 #rank = np.linalg.matrix_rank(farray, 1.0e-10)
 #print(rank)
 #print(decomps.EigDecomp(farray))
 
-'''
-print(q)
-print(r)
-print(np.dot(q,r)) 
-'''
 
-'''
-ranks = np.linalg.matrix_rank(farray)
-for i in range(ranks):
-    e = np.linalg.matrix_rank(farray)-1-i
-    newRank = decomps.approxDim(farray,e,0)
-    #print(ranks)
-    #print(newRank)
-    if(newRank != ranks):
-        print(str(newRank) + " , " + str(e))
-        rank = newRank
-'''
+
+
+#structure score reflects how much error is required to reduce the amount of information within a matrix. The more random,
+#the less reduction is required to remove information. 
+#Take the amount of error needed to reduce the rank by 1
+#find the relitive percent of information that rank holds. 1/total ranks of original matrix
+#That percentage of the matrixes total information is stored in the information lost: ie, the error represents a loss of that much of the total information. 
+n = decomps.erInFirstRank(farray,1.0e-15,0,100)
+
